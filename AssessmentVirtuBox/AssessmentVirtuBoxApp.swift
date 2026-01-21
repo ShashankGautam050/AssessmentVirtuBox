71174c110419a70f5ae0ad00d79bb895db2f4e37
Shashank Gautam <shashank320722@gmail.com>
2026-01-22 00:26:06 +0530

    Initial Commit

---
+++ AssessmentVirtuBox/AssessmentVirtuBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentVirtuBoxApp.swift
+//  AssessmentVirtuBox
+//
+//  Created by Shashank Gautam on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentVirtuBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
